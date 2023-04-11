7a1ac7f79a1c85724c7d7b1ecfe38d687c4db48f
Amit Batra <75168062+amitbatra31@users.noreply.github.com>
2023-04-11 11:26:03 +0530

    Add files

--- main.py
+++ main.py
@@ -1,16 +1,57 @@
-# This is a sample Python script.
+from flask import Flask, jsonify,request
+from flask_restful import Api, Resource, reqparse
+import pickle
+import numpy as np
+import json
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+# from sklearn.externals import joblib
+app = Flask(__name__)
+api = Api(app)
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Create parser for the payload data
+parser = reqparse.RequestParser()
+parser.add_argument('data')
+
+# Define how the api will respond to the post requests
+class IrisClassifier(Resource):
+    def post(self):
+        print("Header is", request.headers.get('Content-Type'))
+        args = parser.parse_args()
+        X = np.array(json.loads(args['data']))
+        prediction = model.predict(X)
+        return jsonify(prediction.tolist())
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+api.add_resource(IrisClassifier, '/iris')
 
 
-# Press the green button in the gutter to run the script.
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    if request.method == 'POST':
+
+        print(request.args.get('N'))
+
+        test_d = {
+            "N": float(request.args.get('N')),
+            "P": float(request.args.get('P')),
+            "K": float(request.args.get('K')),
+            "temperature": float(request.args.get('temperature')),
+            "humidity": float(request.args.get('humidity')),
+            "ph": float(request.args.get('ph')),
+            "rainfall":float(request.args.get('rainfall'))
+        }
+
+        test_input = pd.DataFrame(test_d, index=[0])
+        my_prediction = model.predict(test_input)
+        return jsonify(my_prediction.tolist())
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Load model
+    # with open('model.pickle', 'rb') as f:
+    #     model = pickle.load(f)
+    with open('model.pickle', 'rb') as f:
+        model = pickle.load(f)
+    app.run(debug=True,port=3000)
